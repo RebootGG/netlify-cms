0c891db4ca568f6eb06c3a474c8dacba42b47f08
Aymar TISSEDRE <aymar.tissedre@capgemini.com>
2020-11-22 23:37:09 +0100

    Added list of blog posts on homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,33 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  const pagesArray = data.allMarkdownRemark.edges.map((edge) => {
+    const { path, title } = edge.node.frontmatter
+    console.log(path)
+    return <li><Link to={`/${path}`}>{title}</Link></li>
+  })
+
+  return <div><p>List of blog posts</p>
+    <ul>
+      {pagesArray}
+    </ul>
+  </div>
 }
+
+export const pageQuery = graphql`
+  query Query {
+  allMarkdownRemark {
+    edges {
+      node {
+        frontmatter {
+          title
+          path
+          date
+        }
+      }
+    }
+  }
+}
+
+`
